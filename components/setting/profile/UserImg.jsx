a0f1b40d65a1af8f74f4e04befc0eb173c4b6dd8
NamkhaengV <suchanart.k64@rsu.ac.th>
2023-05-04 21:12:03 +0700

    feat: user update image

--- components/setting/profile/UserImg.jsx
+++ components/setting/profile/UserImg.jsx
@@ -5,8 +5,53 @@ import { useContext, useEffect, useState } from "react";
 import { GlobalContext } from "@/lib/AppContext";
 import useAxios from "@/lib/useAxios";
 import axios from "axios";
+import { bufferToUrl, bufferToBlobUrl } from '@/lib/Image';
 
-const props = {
+const UserImg = () => {
+  const {user} = useContext(GlobalContext);
+  const { user_id, user_image } = user;
+
+  const [file, setFile] = useState(null);
+
+  const [error, setError] = useState("");
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    const { user_id,  } = user;
+    handleImage(user_id);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleImage = async () => {
+    const formData = new FormData();
+    formData.append("user_img", file);
+    try {
+      const res = await useAxios.put(`/users/${user_id}`, formData);
+      message.success("Update Profile Image Successfully");
+      setIsModalOpen(false);
+
+      console.log(res);
+    } catch (e) {
+      if (axios.isAxiosError(e)) {
+        const status = e.response?.status;
+        if (status === 404) {
+          setError("This user not found");
+          message.open({
+            type: "error",
+            content: "Something went wrong!  😱",
+          });
+        }
+      } else setError("Something went wrong.");
+    }
+  };
+  
+
+  const props = {
   beforeUpload: (file) => {
     const isJpgOrPng = file.type === "image/jpeg" || file.type === "image/png";
     if (!isJpgOrPng) {
@@ -16,27 +61,15 @@ const props = {
   },
   onChange: (info) => {
     console.log(info.fileList);
+    const { status } = info.file;
+    if (status === "done") {
+      message.success(`${info.file.name} file uploaded successfully.`);
+      setFile (info.file.originFileObj);
+    } else if (status === "error") {
+      message.error(`${info.file.name} file upload failed.`);
+    }
   },
 };
-
-const UserImg = () => {
-  const {user} = useContext(GlobalContext);
-  const { user_id, user_image } = user;
-
-  const [error, setError] = useState("");
-
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const showModal = () => {
-    setIsModalOpen(true);
-  };
-  const handleOk = () => {};
-  const handleCancel = () => {
-    setIsModalOpen(false);
-  };
-
-  const handleImage = async (user_id, user_image) => {
-    
-  };
 
   return (
     <>
